425c91e90dd29fc72233b4917258d34448802a76
SamS <sscheuerman@student.cccs.edu>
2018-04-25 20:41:21 -0600

    Updated Schedule Methods
    Added setters and getters. Removed an unused method. Cleaned the fill method to work in main code environment using global constants. Added a temporary constant for the max course size until a global constant is created.

--- Schedule.h
+++ Schedule.h
@@ -22,83 +22,137 @@ checks for 0's when scheduling)
 -Method for outputing schedule, currently outputs arrays using overloaded <<
 *******************************/
 
-const int NUMROOMZ = 100;
+const int COURSEMAX = 750;
 
 class Schedule
 {
 private:
-	string rooms[NUMROOMZ];
-	Course * schedule[91][NUMROOMZ];
+	string rooms[RMMAX];
+	Course * schedule[91][RMMAX];
 
+	Course **focus;
 public:
 	Schedule( )
 	{
-		for ( int i = 0; i < NUMROOMZ; i++ )
+		for ( int i = 0; i < RMMAX; i++ )
 		{
 			rooms[i] = "NULL";
 		}
 		for ( int i = 0; i < 91; i++ )
 		{
-			for ( int j = 0; j < NUMROOMZ; j++ )
+			for ( int j = 0; j < RMMAX; j++ )
 			{
 				schedule[i][j] = nullptr;
 			}
 		}
+
+		focus = nullptr;
 	}
 
-	//Adds a single course to the schedule, cannot check for conflicts
-	void add( Room &room, Course &course )
+	Schedule( string rmArray[RMMAX] )
 	{
-		cout << "In add" << endl;
-		int index = 0;
-		string roomnum = room.getroomName( );
-		while ( index < NUMROOMZ )
+		for ( int i = 0; i < RMMAX; i++ )
 		{
-			if ( rooms[index] == "NULL" )
+			cout << rmArray[i];
+			rooms[i] = rmArray[i];
+			cout << "Room added: " << rooms[i];
+		}
+
+		for ( int i = 0; i < 91; i++ )
+		{
+			for ( int j = 0; j < RMMAX; j++ )
 			{
-				cout << "Adding room" << endl;
-				rooms[index] = roomnum;
-				//room.setIndex( index );
-				//course.setIndex( index );
-				cout << rooms[index] << endl;
-				break;
-			}
-			else if ( rooms[index] == roomnum )
-			{
-				cout << "Found room" << endl;
-				//course.setIndex( index );
-				cout << rooms[index] << endl;
-				break;
-			}
-			else
-			{
-				index++;
+				schedule[i][j] = nullptr;
 			}
 		}
-		bool full = true;
-		//int crn = Course.getCRN( );
-		for ( int row = 0; row < 91; row++ )
+
+		focus = nullptr;
+	}
+
+	~Schedule( )
+	{
+		delete focus;
+	}
+
+	////////////////////SETTERS/////////////////////
+
+	void setRoomArray( string rmArray[RMMAX] )
+	{
+		for ( int i = 0; i < RMMAX; i++ )
 		{
-			if ( schedule[row][index] == nullptr )
-			{
-				cout << "Setting CRN" << endl;
-				schedule[row][index] = &course;
-				full = false;
-				cout << schedule[row][index] << endl;
-				break;
-			}
-		}
-		if ( full )
-		{
-			cout << roomnum[index] << " was full" << endl;
+			rooms[i] = rmArray[i];
 		}
 	}
+
+	void setSchedule( Room rms[ ], Course crs[ ] )
+	{
+		fill( rms, crs );
+	}
+
+	void setSingleRoom( string rm, int index )
+	{
+		rooms[index] = rm;
+	}
+
+	void setSingleCourse( Course *crs, int row, int col )
+	{
+		schedule[row][col] = crs;
+	}
+
+	/////////////////////GETTERS///////////////////////////
+
+	string getSingleRoom( int index )
+	{
+		return rooms[index];
+	}
+
+	Course * getCourse( int row, int col )
+	{
+		return schedule[row][col];
+	}
+
+	string * getRooms( )
+	{
+		return rooms;
+	}
+
+	void getSchedule( Course * toReturn[91][RMMAX] )
+	{
+		for ( int i = 0; i < 91; i++ )
+		{
+			for ( int j = 0; j < RMMAX; j++ )
+			{
+				toReturn[i][j] = schedule[i][j];
+			}
+		}
+	}
+
+	void getColumn( Course * toReturn[91], int index )
+	{
+		for ( int i = 0; i < 91; i++ )
+		{
+			toReturn[i] = schedule[i][index];
+		}
+	}
+
+	Course * getFocusedColumn( int index )
+	{
+		delete focus;
+		focus = new Course*[91];
+		for ( int i = 0; i < 91; i++ )
+		{
+			focus[i] = schedule[i][index];
+		}
+		return *focus;
+	}
+
+	////////////////////METHODS////////////////////////////
 
 	//Returns the index of a room in the rooms array
 	int findRoom( string room )
 	{
 		int index = 0;
-		while ( index < NUMROOMZ )
+		while ( index < RMMAX )
 		{
 			if ( rooms[index] == "NULL" )
 			{
@@ -119,16 +173,16 @@ public:
 	}
 
 	//Schedules all rooms at once, methods are outdated
-	void fill( Room Rooms[ ], int rsize, Course Sections[ ], int ssize, int positions[ ], int psize )
+	void fill( Room Rooms[ ], Course Sections[ ])
 	{
 		ofstream fout;
 		fout.open( "ScheduleConflicts.txt" );
 		string sroom, room;
-		for ( int i = 0; i < rsize; i++ )
+		for ( int i = 0; i < RMMAX; i++ )
 		{
 			room = Rooms[i].getroomName( );
 			//cout << "Checking for room " << room << endl;
-			for ( int index = 0; index < ssize; index++ )
+			for ( int index = 0; index < COURSEMAX; index++ )
 			{
 				sroom = Sections[index].getRoom( );
 				//cout << "sroom is " << sroom << endl;
@@ -152,33 +206,13 @@ public:
 						}
 						else
 						{
-							//cout << "Row " << row << " was full" << endl;
-							int ref = (schedule[row][loc]->getCrn( ) - 60000);
-							if ( ref >= 0 && ref < psize )
+							bool conflict = Sections[index].conflictCheck( *schedule[row][loc] );
+							if ( conflict )
 							{
-								//cout << "Checking for conflict" << endl;
-								//cout << Sections[index].getCRN( ) << " " << Sections[positions[ref]].getCRN( ) << endl;
-								bool conflict = Sections[index].conflictCheck( Sections[positions[ref]] );
-								//cout << "Conflict was " << conflict << endl;
-								/*if ( Sections[index].hasLink( ) )
-								{
-								Course* link = Sections[index].getLink( );
-								if ( link->getRoom( ) == room )
-								{
-								conflict += link->conflictCheck( Sections[positions[ref]] );
-								}
-								}*/
-								if ( conflict )
-								{
-									cout << Sections[index].getCRN( ) << " conflicts with " << schedule[row][loc]->getCRN( ) << endl;
-									fout << Sections[index].getCRN( ) << endl;
-									full = false;
-									break;
-								}
-							}
-							else
-							{
-								cout << "Reference error. CRN was out of scope" << endl;
+								cout << Sections[index].getCrn( ) << " conflicts with " << schedule[row][loc]->getCrn( ) << endl;
+								fout << Sections[index].getCrn( ) << endl;
+								full = false;
+								break;
 							}
 							row++;
 						}
@@ -200,7 +234,7 @@ public:
 	{
 		int index = 0;
 		string roomnum = Room->getroomName( );
-		while ( index < NUMROOMZ )
+		while ( index < RMMAX )
 		{
 			if ( rooms[index] == "NULL" )
 			{
@@ -276,14 +310,14 @@ public:
 	//Outputs entire schedule array in CSV friendly format
 	friend ostream &operator<< ( ostream &stream, Schedule toOutput )
 	{
-		for ( int i = 0; i < NUMROOMZ; i++ )
+		for ( int i = 0; i < RMMAX; i++ )
 		{
 			stream << toOutput.rooms[i] << ',';
 		}
 		stream << endl;
 		for ( int i = 0; i < 91; i++ )
 		{
-			for ( int index = 0; index < NUMROOMZ; index++ )
+			for ( int index = 0; index < RMMAX; index++ )
 			{
 				if ( toOutput.schedule[i][index] != nullptr )
 				{
